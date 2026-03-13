ec34aa4e40b0c09ec884c045ed9dd8706c5b69aa
elamcheliyan <elamcheliyan@gmail.com>
2026-03-13 11:28:55 +0100

    optimized the code according to the module parameters

--- base/variables.tf
+++ base/variables.tf
@@ -10,7 +10,6 @@ variable "resource_group_name_prefix" {
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-
 variable "web_app_name" {
   description = "app name"
   type        = string
